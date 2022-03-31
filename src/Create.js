9940d4c7c836c85762dad317562d99b79994f981
Ian Kamau <78727737+Kamau-ke@users.noreply.github.com>
2022-03-30 23:00:02 -0700

    Add files via upload

---
+++ src/Create.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+const Create = () => {
+  const [title, setTitle]=useState(' ')
+  const [body, setBody]=useState(' ')
+  const [author, setAuthor]=useState('mario')
+
+  const [isPending, setIsPending]=useState(false);
+  const history=useHistory();
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const blog={ title, body, author };
+
+    setIsPending(true)
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then (() =>{
+      console.log('new blog added')
+      setIsPending(false)
+      history.push('/')
+    })
+     
+  }
+  return (
+    <div className='create'>
+        <h2>Add new blog</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <label>Blog title: </label>
+          <input
+           type="text"
+           required
+           value={title}
+           onChange={(e) =>setTitle(e.target.value)}
+          
+          />
+
+          <label>Blog body: </label>
+          <textarea 
+          required
+          value={ body }
+          onChange={(e)=>setBody(e.target.value)}
+          ></textarea>
+
+          <label>Blog author: </label>
+          <select
+         value={author} 
+         onChange={(e) =>setAuthor(e.target.value)}
+          >
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+        
+       {!isPending &&<button>Add blog</button>} 
+       {isPending &&<button disabled>Adding blog...</button>} 
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
+        </form>
+    
+        </div>
+  )
+}
+
+export default Create
---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>The Dojo</h1>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/create">New Blog</Link>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
